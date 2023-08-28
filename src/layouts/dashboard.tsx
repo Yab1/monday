015083e33303aeaccd7b17b7e4b7f04301fe209c
Yab1 <yabilisanu@gmail.com>
2023-08-28 17:42:30 +0300

    Dashboard navbar styled

--- src/layouts/dashboard.tsx
+++ src/layouts/dashboard.tsx
@@ -1,5 +1,13 @@
+import DashboardNavbar from "../widgets/dashboard-navbar";
 import Sidenav from "../widgets/layout/sidenav";
 
 export default function Dashboard() {
-  return <Sidenav />;
+  return (
+    <div className="min-h-screen bg-blue-gray-50/50">
+      <Sidenav />
+      <div className="p-4 xl:ml-80 min-h-screen">
+        <DashboardNavbar />
+      </div>
+    </div>
+  );
 }
