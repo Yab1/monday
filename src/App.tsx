1821f95fe00ef168d0881ebee67cf95ce7993743
Yab1 <yabilisanu@gmail.com>
2023-08-28 17:12:40 +0300

    Dashboard rendered

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import Dashboard from "./layouts/dashboard";
+
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/dashboard/*" element={<Dashboard />} />
+      <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
+    </Routes>
+  );
 }
 
 export default App;
