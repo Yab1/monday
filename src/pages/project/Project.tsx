3fe88aea9c373958a4c5a0920a1f281f20a5ec9a
Yab1 <yabilisanu@gmail.com>
2023-09-25 18:56:21 +0300

    Project Table styled

--- src/pages/project/Project.tsx
+++ src/pages/project/Project.tsx
@@ -1,5 +1,80 @@
+import { useState } from "react";
+import {
+  Typography,
+  Tabs,
+  TabsHeader,
+  TabsBody,
+  Tab,
+  TabPanel,
+  Button,
+} from "@material-tailwind/react";
+import { PlusIcon } from "@heroicons/react/24/solid";
+import { ProjectDetailsTable } from "@/widgets";
+
 function Project() {
-  return <section>Project</section>;
+  const [activeTab, setActiveTab] = useState("project 1");
+  const data = [
+    {
+      label: "Project 1",
+      value: "project 1",
+      desc: `It really matters and then like it really doesn't matter.
+      What matters is the people who are sparked by it. And the people 
+      who are like offended by it, it doesn't matter.`,
+    },
+    {
+      label: "Project 2",
+      value: "project 2",
+      desc: `It really matters and then like it really doesn't matter.
+      What matters is the people who are sparked by it. And the people 
+      who are like offended by it, it doesn't matter.`,
+    },
+    {
+      label: "Project 3",
+      value: "project 3",
+      desc: `It really matters and then like it really doesn't matter.
+      What matters is the people who are sparked by it. And the people 
+      who are like offended by it, it doesn't matter.`,
+    },
+  ];
+  return (
+    <section className="flex flex-col gap-3">
+      <Typography variant="h3" className="capitalize">
+        {activeTab}
+      </Typography>
+      <Tabs value={activeTab}>
+        <TabsHeader
+          className="rounded-none border-b border-blue-gray-50 bg-transparent p-0"
+          indicatorProps={{
+            className:
+              "bg-transparent border-b-2 border-gray-900 shadow-none rounded-none",
+          }}
+        >
+          {data.map(({ label, value }) => (
+            <Tab
+              key={value}
+              value={value}
+              onClick={() => setActiveTab(value)}
+              className={`w-fit px-8 py-3 ${
+                activeTab === value ? "text-gray-900" : ""
+              }`}
+            >
+              {label}
+            </Tab>
+          ))}
+          <Button className="px-8 py-3 w-fit" variant="text">
+            <PlusIcon className="h-5 aspect-square" />
+          </Button>
+        </TabsHeader>
+        <TabsBody>
+          {data.map(({ value, desc }) => (
+            <TabPanel key={value} value={value}>
+              <ProjectDetailsTable />
+            </TabPanel>
+          ))}
+        </TabsBody>
+      </Tabs>
+    </section>
+  );
 }
 
 Project.displayName = "/src/pages/project/project.tsx";
--- src/widgets/tables/ProjectDetailsTable.tsx
+++ src/widgets/tables/ProjectDetailsTable.tsx
@@ -1,5 +1,145 @@
+import {
+  MagnifyingGlassIcon,
+  ChevronUpDownIcon,
+} from "@heroicons/react/24/outline";
+import { PencilIcon, UserPlusIcon } from "@heroicons/react/24/solid";
+import {
+  Card,
+  CardHeader,
+  Input,
+  Typography,
+  Button,
+  CardBody,
+  Chip,
+  CardFooter,
+  Tabs,
+  TabsHeader,
+  Tab,
+  Avatar,
+  IconButton,
+  Tooltip,
+} from "@material-tailwind/react";
+
+const TABS = [
+  {
+    label: "All",
+    value: "all",
+  },
+  {
+    label: "Monitored",
+    value: "monitored",
+  },
+  {
+    label: "Unmonitored",
+    value: "unmonitored",
+  },
+];
+
+const TABLE_HEAD = ["Task", "Owner", "Due Date", "Status", ""];
+
+const TABLE_ROWS = [
+  {
+    id: "1",
+    taskLabel: "John Michael",
+    owner: "",
+    dueDate: "23/04/18",
+    status: "",
+  },
+];
+
 function ProjectDetailsTable() {
-  return <></>;
+  return (
+    <Card className="w-full">
+      <CardBody className="p-2 rounded-lg">
+        <table className="w-full min-w-max table-auto text-left rounded-xl">
+          <thead>
+            <tr>
+              {TABLE_HEAD.map((head, index) => (
+                <th
+                  key={head}
+                  className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50"
+                >
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="flex items-center justify-between gap-2 font-normal leading-none opacity-70"
+                  >
+                    {head}{" "}
+                    {index !== TABLE_HEAD.length - 1 && (
+                      <ChevronUpDownIcon strokeWidth={2} className="h-4 w-4" />
+                    )}
+                  </Typography>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {TABLE_ROWS.map(
+              ({ id, taskLabel, owner, dueDate, status }, index) => {
+                const isLast = index === TABLE_ROWS.length - 1;
+                const classes = isLast
+                  ? "p-4"
+                  : "p-4 border-b border-blue-gray-50";
+
+                return (
+                  <tr key={id}>
+                    <td className={classes}>
+                      <div className="flex items-center gap-3">
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal"
+                        >
+                          {taskLabel}
+                        </Typography>
+                      </div>
+                    </td>
+                    <td className={classes}>
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal"
+                      >
+                        {owner}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal"
+                      >
+                        {dueDate}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <div className="w-max">
+                        <Chip
+                          variant="ghost"
+                          size="sm"
+                          value={status}
+                          color={"green"}
+                        />
+                      </div>
+                    </td>
+                    <td className={classes}>
+                      <Tooltip content="Edit User">
+                        <IconButton variant="text">
+                          <PencilIcon className="h-4 w-4" />
+                        </IconButton>
+                      </Tooltip>
+                    </td>
+                  </tr>
+                );
+              }
+            )}
+          </tbody>
+        </table>
+      </CardBody>
+    </Card>
+  );
 }
+
+ProjectDetailsTable.displayName = "/src/widgets/tables/ProjectDetailsTable.tsx";
 
 export default ProjectDetailsTable;
