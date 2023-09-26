86bdd354c181f57465866d70e6e93e82465804cc
Yab1 <yabilisanu@gmail.com>
2023-09-26 20:17:49 +0300

    Redux: Set up Provider.

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "@material-tailwind/react";
+import { store } from "@/redux";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
