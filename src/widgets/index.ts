ff37494489e3a523f908401fec1bc7d71fc9ce42
Yab1 <yabilisanu@gmail.com>
2023-09-29 20:52:57 +0300

    Refactor: Corrected component import.

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -2,4 +2,3 @@ export { default as DashboardNavbar } from "./DashboardNavbar";
 export { default as StatisticsChart } from "./charts/StatisticsChart";
 export { default as SideNav } from "./layout/SideNav";
 export { default as ProjectOverviewTable } from "./tables/ProjectOverviewTable";
-export { default as TaskMenu } from "./menu/TaskMenu";
