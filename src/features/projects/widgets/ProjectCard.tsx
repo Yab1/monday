c70347e2c67258ca698bf1124087e401dae2cbde
Yab1 <yabilisanu@gmail.com>
2023-10-12 18:43:17 +0300

    Feature: implemented changing status

--- src/features/projects/widgets/ProjectCard.tsx
+++ src/features/projects/widgets/ProjectCard.tsx
@@ -18,22 +18,27 @@ import {
 } from "@heroicons/react/24/outline";
 import { toggleDialog } from "@/common";
 import { ConfirmationDialog } from "@/widgets";
-import { toggleConfirmationDialog } from "@/common/uiSlice";
-import { DeleteType } from "@/enum";
+import { toggleConfirmationDialog } from "@/common";
+import { DeleteEnum } from "@/enum";
+import { updateProject } from "../slice";
 
 function ProjectCard() {
-  const { label, status, description } = useAppSelector(
-    (state) => state.selectedProject
-  );
+  const selectedProject = useAppSelector((state) => state.selectedProject);
 
-  // const { isEditDialogOpen } = useAppSelector((state) => state.ui);
   const dispatch = useAppDispatch();
+
+  const chipColor =
+    selectedProject.status === "Pending"
+      ? "blue-gray"
+      : selectedProject.status === "Completed"
+      ? "green"
+      : "yellow";
 
   return (
     <Fragment>
       <div className="mt-10 flex items-cent1er">
-        <Typography variant="h3" className="capitalize mr-5">
-          {label}
+        <Typography variant="h3" className="capitalize mr-4">
+          {selectedProject.label}
         </Typography>
 
         <Tooltip
@@ -41,24 +46,67 @@ function ProjectCard() {
           className="border border-blue-gray-50 bg-white px-4 py-3 shadow-xl shadow-black/10"
           content={
             <Typography variant="small" color="blue-gray">
-              {description}
+              {selectedProject.description}
             </Typography>
           }
         >
-          <Button variant="text" className="p-0">
-            <InformationCircleIcon className="w-5 aspect-square" />
-          </Button>
+          <InformationCircleIcon className="w-5" />
         </Tooltip>
 
         <div className="flex gap-3 ml-auto mr-5">
-          <Chip
-            color="green"
-            value={status}
-            className="p-1 px-2 text-xs font-body capitalize"
-          />
           <Menu>
             <MenuHandler>
-              <Button variant="text" className="p-0">
+              <Chip
+                color={chipColor}
+                value={selectedProject.status}
+                className="py-1 px-3 text-xs font-body capitalize rounded-xl h-fit self-center cursor-pointer"
+              />
+            </MenuHandler>
+            <MenuList className="bg-gray-200 shadow-none flex flex-col gap-2 w-fit">
+              <MenuItem
+                className="bg-blue-gray-700 text-white w-fit text-xs font-body py-1 px-3 rounded-full"
+                onClick={() =>
+                  dispatch(
+                    updateProject({
+                      ...selectedProject,
+                      status: "Pending",
+                    })
+                  )
+                }
+              >
+                Pending
+              </MenuItem>
+              <MenuItem
+                className="bg-yellow-500 text-blue-gray-900 w-fit text-xs font-body py-1 px-3 rounded-full"
+                onClick={() =>
+                  dispatch(
+                    updateProject({
+                      ...selectedProject,
+                      status: "In Progress",
+                    })
+                  )
+                }
+              >
+                In Progress
+              </MenuItem>
+              <MenuItem
+                className="bg-green-500 text-white w-fit text-xs font-body py-1 px-3 rounded-full"
+                onClick={() =>
+                  dispatch(
+                    updateProject({
+                      ...selectedProject,
+                      status: "Completed",
+                    })
+                  )
+                }
+              >
+                Completed
+              </MenuItem>
+            </MenuList>
+          </Menu>
+          <Menu>
+            <MenuHandler>
+              <Button variant="text" className="p-1 rounded-full h-fit my-auto">
                 <EllipsisHorizontalIcon className="w-6 aspect-square" />
               </Button>
             </MenuHandler>
@@ -73,9 +121,9 @@ function ProjectCard() {
                 </Typography>
               </MenuItem>
               <MenuItem
-                className="flex items-center gap-2"
+                className="flex items-center gap-2 border"
                 onClick={() =>
-                  dispatch(toggleConfirmationDialog(DeleteType.Project))
+                  dispatch(toggleConfirmationDialog(DeleteEnum.Project))
                 }
               >
                 <TrashIcon className="w-4 aspect-square" />
