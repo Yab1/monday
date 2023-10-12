11d68a74df7ce6dbf23945fa74792123434d3bd8
Yab1 <yabilisanu@gmail.com>
2023-10-12 14:09:07 +0300

    Cleanup: removing unused code

--- src/common/uiSlice.ts
+++ src/common/uiSlice.ts
@@ -1,5 +1,5 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
-import { UIStates, ConfirmationState } from "@/interfaces";
+import { UIStates } from "@/interfaces";
 import { DeleteType } from "@/enum";
 
 const initialState: UIStates = {
