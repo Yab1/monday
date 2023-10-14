8d2607d9bd100147f40d1abd34a274ea2256b674
Yab1 <yabilisanu@gmail.com>
2023-10-14 18:57:35 +0300

    Cleanup: Removed unused actions

--- src/features/projects/slice/projectsSlice.ts
+++ src/features/projects/slice/projectsSlice.ts
@@ -1,8 +1,8 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { initialState } from "@/data";
-import { Project } from "@/interfaces";
 import { ActionEnum, TargetEnum } from "@/enum";
 import alterDictionary from "@/dictionaries/alterDictionary";
+// import { Project } from "@/interfaces";
 
 export const projectSlice = createSlice({
   name: "projectSlice",
@@ -18,43 +18,11 @@ export const projectSlice = createSlice({
       }>
     ) => {
       const { target, id, actionType, payload } = action.payload;
+
       return alterDictionary[target][actionType](id, state, payload);
     },
-    addProject: (state, action: PayloadAction<Project>) => {
-      return [...state, action.payload];
-    },
-    removeProject: (state, action: PayloadAction<string>) => {
-      const updatedProjects = state.filter(
-        (project) => project.id !== action.payload
-      );
-      return updatedProjects;
-    },
-    updateProject: (state, action: PayloadAction<Project>) => {
-      const updatedProjects = state.map((project) => {
-        if (project.id === action.payload.id) {
-          return action.payload;
-        }
-        return project;
-      });
-      return updatedProjects;
-    },
-    addTask: () => {},
-    removeTask: () => {},
-    addGroup: () => {},
-    removeGroup: () => {},
-    updateTask: () => {},
   },
 });
 
-export const {
-  alterRecord,
-  addProject,
-  addGroup,
-  addTask,
-  removeProject,
-  removeGroup,
-  removeTask,
-  updateProject,
-  updateTask,
-} = projectSlice.actions;
+export const { alterRecord } = projectSlice.actions;
 export default projectSlice.reducer;
