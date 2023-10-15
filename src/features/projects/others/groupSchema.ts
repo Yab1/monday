4cf79c05feec88fb91fc3b7c5ed02f85808d32eb
Yab1 <yabilisanu@gmail.com>
2023-10-15 17:49:11 +0300

    Fix: Spelling

--- src/features/projects/others/groupSchema.ts
+++ src/features/projects/others/groupSchema.ts
@@ -2,7 +2,7 @@ import { object, string } from "yup";
 
 const groupSchema = object({
   groupName: string()
-    .required("Project title is required")
+    .required("Group name is required")
     .min(4, "Group name must be at least 4 characters")
     .max(60, "Group name must be at most 60 characters"),
 });
