a1156b5ba94b31cc201151ac7c9117f622d834b0
Yab1 <yabilisanu@gmail.com>
2023-10-15 14:11:47 +0300

    Refactor: added timeStamp and  creator properties

--- src/enum/index.ts
+++ src/enum/index.ts
@@ -2,3 +2,4 @@ export { default as ActionEnum } from "./ActionEnum";
 export { default as StatusEnum } from "./StatusEnum";
 export { default as TargetEnum } from "./TargetEnum";
 export { default as ToggleableEnum } from "./ToggleableEnum";
+export { default as UserTypeEnum } from "./UserTypeEnum";
