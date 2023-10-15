165e9bc8fe1b68afdc8399e207f7e1f6ee319cdd
Yab1 <yabilisanu@gmail.com>
2023-10-15 12:40:44 +0300

    Code: a useEffect to set selectedProject state

--- src/features/projects/ProjectManager.tsx
+++ src/features/projects/ProjectManager.tsx
@@ -10,17 +10,23 @@ import { selectProject } from "./slice";
 
 function ProjectManager() {
   const projects = useAppSelector((state) => state.projects);
+  const selectedProject = useAppSelector((state) => state.selectedProject);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    if (projects.length > 0) {
+    if (Boolean(selectedProject.id)) {
+      const currentProject = projects.filter(
+        (project) => project.id === selectedProject.id
+      );
+      dispatch(selectProject(currentProject[0]));
+    } else if (projects.length > 0) {
       dispatch(selectProject(projects[0]));
     }
   }, [projects]);
 
   return (
     <section className="flex flex-col gap-8">
-      {projects.length > 0 ? (
+      {Boolean(selectedProject.id) ? (
         <Fragment>
           <ProjectCard />
           <ProjectTabs />
