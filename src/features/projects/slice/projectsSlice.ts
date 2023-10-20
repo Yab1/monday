ed1caa75092b4f7c04aa8edc4737f4e549cc0342
Yab1 <yabilisanu@gmail.com>
2023-10-20 19:58:04 +0300

    Redux:Added a new Payload

--- src/features/projects/slice/projectsSlice.ts
+++ src/features/projects/slice/projectsSlice.ts
@@ -2,24 +2,24 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { initialState } from "@/data";
 import { ActionEnum, TargetEnum } from "@/enum";
 import alterDictionary from "@/dictionaries/alterDictionary";
-// import { Project } from "@/interfaces";
+import { Project, UserInterface } from "@/interfaces";
 
 export const projectSlice = createSlice({
   name: "projectSlice",
   initialState,
   reducers: {
     alterRecord: (
-      state,
+      _,
       action: PayloadAction<{
+        id: string;
+        data: Project[] | UserInterface[];
+        payload?: Object;
         target: TargetEnum;
         actionType: ActionEnum;
-        id: string;
-        payload?: Object;
       }>
     ) => {
-      const { target, id, actionType, payload } = action.payload;
-
-      return alterDictionary[target][actionType](id, state, payload);
+      const { id, data, payload, target, actionType } = action.payload;
+      return alterDictionary[target][actionType](id, data, payload);
     },
   },
 });
