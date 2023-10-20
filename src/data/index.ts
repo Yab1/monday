2a43486c7c3f649da35436cb622ff39d999a9645
Yab1 <yabilisanu@gmail.com>
2023-10-20 19:50:01 +0300

    Redux: Reducer to manage user accounts

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,7 @@
 export { default as projectsTableData } from "./projects-table-data";
 export { default as statisticsChartsData } from "./statistics-charts-data";
 export { default as initialState } from "./projects-data";
+export { default as userData } from "./user-data";
 // export * from "./orders-overview-data";
 // export * from "./platform-settings-data";
 // export * from "./conversations-data";
