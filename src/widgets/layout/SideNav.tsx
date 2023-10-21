4cad165fedecdfd65cfecc45f527b306d6860c9b
Yab1 <yabilisanu@gmail.com>
2023-10-21 16:15:08 +0300

    Removed Nav close icon

--- src/widgets/layout/SideNav.tsx
+++ src/widgets/layout/SideNav.tsx
@@ -1,23 +1,19 @@
 import { Fragment } from "react";
 import { Link, NavLink } from "react-router-dom";
-import { XMarkIcon } from "@heroicons/react/24/outline";
-import { Button, IconButton, Typography } from "@material-tailwind/react";
+import { Button, Typography } from "@material-tailwind/react";
 import { primaryRoutes, secondaryRoutes } from "@/routes";
-import { useAppSelector, useAppDispatch } from "@/hooks";
-import { toggler } from "@/slices";
-import { ToggleableEnum } from "@/enum";
+import { useAppSelector } from "@/hooks";
 function Sidenav() {
   const {
     toggleable: { darkMode, sideNav },
   } = useAppSelector((state) => state.ui);
-  const dispatch = useAppDispatch();
 
   return (
     <Fragment>
       {sideNav ? (
         <aside className="fixed inset-0 z-50 my-4 ml-4 h-[calc(100vh-32px)] w-72 rounded-xl transition-transform duration-300 xl:translate-x-0 border bg-white shadow-lg dark:bg-gradient-to-br from-blue-gray-800 to-blue-gray-900 ">
           <div className="relative border-b border-blue-gray-50 dark:border-white/20">
-            <Link to="/" className="flex items-center gap-4 py-6 px-8">
+            <Link to="/" className="flex items-center gap-4 px-8 py-6">
               <Typography
                 variant="h6"
                 className="text-blue-gray-900 dark:text-white"
@@ -25,20 +21,10 @@ function Sidenav() {
                 WorkFlow
               </Typography>
             </Link>
-            <IconButton
-              variant="text"
-              color="white"
-              size="sm"
-              ripple={false}
-              className="absolute right-0 top-0 grid rounded-br-none rounded-tl-none xl:hidden"
-              onClick={() => dispatch(toggler(ToggleableEnum.SideNav))}
-            >
-              <XMarkIcon strokeWidth={2.5} className="h-5 w-5 text-white" />
-            </IconButton>
           </div>
           <div className="m-4">
             {primaryRoutes.map(({ layout, pages }, key) => (
-              <ul key={key} className="mb-4 flex flex-col gap-1">
+              <ul key={key} className="flex flex-col gap-1 mb-4">
                 {pages.map(({ icon, name, path }) => (
                   <li key={name}>
                     <NavLink to={`/${layout}${path}`}>
@@ -71,7 +57,7 @@ function Sidenav() {
             ))}
             <hr className="my-2 border-blue-gray-50" />
             {secondaryRoutes.map(({ layout, pages }, key) => (
-              <ul key={key} className="mb-4 flex flex-col gap-1">
+              <ul key={key} className="flex flex-col gap-1 mb-4">
                 {pages.map(({ icon, name, path }) => (
                   <li key={name}>
                     <NavLink to={`/${layout}${path}`}>
