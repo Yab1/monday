97ec0b9989ae77f103e728191361afda212faa78
Yab1 <yabilisanu@gmail.com>
2023-10-21 21:55:51 +0300

    Redux: Added current user reducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -4,10 +4,12 @@ import {
   projectsReducer,
   selectedProjectReducer,
 } from "@/features/projects/slice";
+import { currentUserReducer } from "@/features/profile/slice";
 
 export const rootReducer = combineReducers({
   auth: authReducer,
   ui: uiReducer,
   projects: projectsReducer,
   selectedProject: selectedProjectReducer,
+  currentUser: currentUserReducer,
 });
