715921daa70148878f7ff7cfe29de9c878cb9806
Yab1 <yabilisanu@gmail.com>
2023-10-21 18:09:37 +0300

    SideNav open by default

--- src/slices/uiSlice.ts
+++ src/slices/uiSlice.ts
@@ -5,7 +5,7 @@ import { ToggleableEnum, TargetEnum } from "@/enum";
 const initialState: UIStates = {
   toggleable: {
     darkMode: false,
-    sideNav: true,
+    sideNav: false,
     editProjectDialog: false,
     addProjectDialog: false,
     addGroup: false,
