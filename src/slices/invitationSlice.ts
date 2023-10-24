61dea9d0a63ab19d35351125607887886be00c8e
Yab1 <yabilisanu@gmail.com>
2023-10-24 14:32:22 +0300

    Redux: create a slice to manage invitation states

---
+++ src/slices/invitationSlice.ts
@@ -0,0 +1,13 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { Invitation } from "@/interfaces";
+
+const initialState = [] as Invitation[];
+
+export const invitationSlice = createSlice({
+  name: "invitations",
+  initialState,
+  reducers: {},
+});
+
+// export const {  } = invitationSlice.actions;
+export default invitationSlice.reducer;
