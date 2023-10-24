4c70476cdef6dc53d9bac7b3b64f4485a03747d5
Yab1 <yabilisanu@gmail.com>
2023-10-24 14:29:52 +0300

    Container for auth forms

--- src/layouts/Auth.tsx
+++ src/layouts/Auth.tsx
@@ -1,19 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 import { primaryRoutes } from "@/routes";
+import background from "@/assets/img/auth-background.svg";
 
 function Auth() {
   return (
-    <div className="relative w-full min-h-screen">
-      <Routes>
-        {primaryRoutes.map(
-          ({ layout, pages }) =>
-            layout === "auth" &&
-            pages.map(({ path, element }) => (
-              <Route path={path} element={element} />
-            ))
-        )}
-      </Routes>
-    </div>
+    <main className="relative grid w-full h-screen grid-cols-12 overflow-hidden">
+      <aside
+        className="absolute inset-0 bg-no-repeat bg-cover -bottom-2 -left-1"
+        style={{ backgroundImage: `url(${background})` }}
+      ></aside>
+      <aside className="relative grid col-start-6 col-span-full bg-light-gray place-content-center">
+        <Routes>
+          {primaryRoutes.map(
+            ({ layout, pages }) =>
+              layout === "auth" &&
+              pages.map(({ path, element }) => (
+                <Route path={path} element={element} />
+              ))
+          )}
+        </Routes>
+      </aside>
+    </main>
   );
 }
 
