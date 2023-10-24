3d2ebb1c5fa9187a55d597d622cbb1604bbb02b1
Yab1 <yabilisanu@gmail.com>
2023-10-24 14:23:32 +0300

    Added a new color and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-gray": "#F8F8F8",
+        "dark-gray": "#595959",
+        "extremely-dark-gray": "#030303",
+        "vibrant-red": "#EA454C",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
