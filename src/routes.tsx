5e639f181d642125c879bd905e437ec182ed0591
Yab1 <yabilisanu@gmail.com>
2023-10-24 14:26:00 +0300

    Added Routes to auth forms

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,13 +7,19 @@ import {
   InboxIcon,
   PowerIcon,
 } from "@heroicons/react/24/solid";
-import { Home, ProjectManager, Profile, Notifications } from "@/features";
-import { SignIn, SignUp } from "@/features/auth";
+// import { Home, ProjectManager, Profile, Notifications } from "@/features";
+import {
+  SignIn,
+  SignUp,
+  VerificationPrompt,
+  ResetPrompt,
+  NewPassword,
+} from "@/features/auth";
 // import { SignIn, SignUp } from "@/pages/auth";
 
-const icon = {
-  className: "w-5 h-5 text-inherit",
-};
+// const icon = {
+//   className: "w-5 h-5 text-inherit",
+// };
 
 interface Page {
   icon?: JSX.Element;
@@ -28,29 +34,29 @@ interface Route {
 }
 
 export const primaryRoutes: Route[] = [
-  {
-    layout: "dashboard",
-    pages: [
-      {
-        icon: <HomeIcon {...icon} />,
-        name: "dashboard",
-        path: "/home",
-        element: <Home />,
-      },
-      {
-        icon: <TableCellsIcon {...icon} />,
-        name: "project",
-        path: "/project",
-        element: <ProjectManager />,
-      },
-      {
-        icon: <UserCircleIcon {...icon} />,
-        name: "profile",
-        path: "/profile",
-        element: <Profile />,
-      },
-    ],
-  },
+  // {
+  //   layout: "dashboard",
+  //   pages: [
+  //     {
+  //       icon: <HomeIcon {...icon} />,
+  //       name: "dashboard",
+  //       path: "/home",
+  //       element: <Home />,
+  //     },
+  //     {
+  //       icon: <TableCellsIcon {...icon} />,
+  //       name: "project",
+  //       path: "/project",
+  //       element: <ProjectManager />,
+  //     },
+  //     {
+  //       icon: <UserCircleIcon {...icon} />,
+  //       name: "profile",
+  //       path: "/profile",
+  //       element: <Profile />,
+  //     },
+  //   ],
+  // },
   {
     layout: "auth",
     pages: [
@@ -64,40 +70,55 @@ export const primaryRoutes: Route[] = [
         path: "/sign-up",
         element: <SignUp />,
       },
+      {
+        name: "create new password",
+        path: "/reset-password",
+        element: <NewPassword />,
+      },
+      {
+        name: "email verification prompt",
+        path: "/verification-prompt",
+        element: <VerificationPrompt />,
+      },
+      {
+        name: "password reset prompt",
+        path: "/reset-prompt",
+        element: <ResetPrompt />,
+      },
     ],
   },
 ];
 
 export const secondaryRoutes: Route[] = [
-  {
-    layout: "dashboard",
-    pages: [
-      {
-        icon: <InboxIcon {...icon} />,
-        name: "inbox",
-        path: "/inbox",
-        element: <InboxIcon />,
-      },
-      {
-        icon: <BellIcon {...icon} />,
-        name: "notifactions",
-        path: "/notifactions",
-        element: <Notifications />,
-      },
-      {
-        icon: <Cog6ToothIcon {...icon} />,
-        name: "setting",
-        path: "/setting",
-        element: <Cog6ToothIcon />,
-      },
-      {
-        icon: <PowerIcon {...icon} />,
-        name: "logout",
-        path: "/logout",
-        element: <PowerIcon />,
-      },
-    ],
-  },
+  // {
+  //   layout: "dashboard",
+  //   pages: [
+  //     {
+  //       icon: <InboxIcon {...icon} />,
+  //       name: "inbox",
+  //       path: "/inbox",
+  //       element: <InboxIcon />,
+  //     },
+  //     {
+  //       icon: <BellIcon {...icon} />,
+  //       name: "notifactions",
+  //       path: "/notifactions",
+  //       element: <Notifications />,
+  //     },
+  //     {
+  //       icon: <Cog6ToothIcon {...icon} />,
+  //       name: "setting",
+  //       path: "/setting",
+  //       element: <Cog6ToothIcon />,
+  //     },
+  //     {
+  //       icon: <PowerIcon {...icon} />,
+  //       name: "logout",
+  //       path: "/logout",
+  //       element: <PowerIcon />,
+  //     },
+  //   ],
+  // },
 ];
 
 export default { primaryRoutes, secondaryRoutes };
