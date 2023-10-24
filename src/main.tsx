2bb5ad70d152f77ca826069514710b32cb595fb7
Yab1 <yabilisanu@gmail.com>
2023-10-24 14:25:31 +0300

    Import statement reorderd

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "@material-tailwind/react";
 import { store } from "@/redux";
-import { Provider } from "react-redux";
+import App from "./App.tsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
