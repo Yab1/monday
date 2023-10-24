d21316bc6d4df0e6d87ee6df0c8f50ad2a22c0dd
Yab1 <yabilisanu@gmail.com>
2023-10-24 21:17:26 +0300

    Added an alert for showing errors

--- src/layouts/Auth.tsx
+++ src/layouts/Auth.tsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import { Alert, Button } from "@material-tailwind/react";
+import { AiOutlineClose } from "react-icons/ai";
 import { primaryRoutes } from "@/routes";
+import { useAppSelector } from "@/hooks";
 import background from "@/assets/img/auth-background.svg";
 
 function Auth() {
+  const [open, setOpen] = useState<boolean>(false);
+  const { error } = useAppSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (error) {
+      setOpen(!open);
+    }
+
+    setTimeout(() => {
+      setOpen(false);
+    }, 4000);
+  }, [error]);
+
   return (
     <main className="relative grid w-full h-screen grid-cols-12 overflow-hidden">
       <aside
@@ -10,6 +27,23 @@ function Auth() {
         style={{ backgroundImage: `url(${background})` }}
       ></aside>
       <aside className="relative grid col-start-6 col-span-full bg-light-gray place-content-center">
+        <Alert
+          open={open}
+          color="red"
+          className="absolute z-10 flex items-center py-1 top-3 w-fit place-self-center"
+          action={
+            <Button
+              variant="text"
+              onClick={() => setOpen(!open)}
+              className="m-0"
+            >
+              <AiOutlineClose />
+            </Button>
+          }
+        >
+          {String(error)}
+        </Alert>
+
         <Routes>
           {primaryRoutes.map(
             ({ layout, pages }) =>
