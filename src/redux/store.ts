2be8fcbfbcacbcb8153bd0a78e54a773fb61f8d5
Yab1 <yabilisanu@gmail.com>
2023-10-24 14:31:12 +0300

    Redux: thunk setup

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -6,7 +6,9 @@ import { rootReducer } from "./rootReducer";
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(thunk).concat(logger),
+    getDefaultMiddleware({ serializableCheck: false })
+      .concat(thunk)
+      .concat(logger),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
