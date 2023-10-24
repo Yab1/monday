987e2da46d04de8521f312b1c0d0a918350b918d
Yab1 <yabilisanu@gmail.com>
2023-10-24 14:29:20 +0300

    Form Schema's

--- src/features/auth/schema/index.ts
+++ src/features/auth/schema/index.ts
@@ -1,2 +1,4 @@
 export { default as signUpSchema } from "./signUpSchema";
 export { default as signInSchema } from "./signInSchema";
+export { default as resetSchema } from "./resetSchema";
+export { default as newPasswordSchema } from "./newPasswordSchema";
