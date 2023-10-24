cbe96bec694f8ca74b23a0748b3b0edb6bd06d31
Yab1 <yabilisanu@gmail.com>
2023-10-24 14:28:27 +0300

    Auth structure

---
+++ src/interfaces/IAuth.ts
@@ -0,0 +1,21 @@
+import { User } from "./index";
+
+type status = "idle" | "loading" | "succeeded" | "failed";
+
+export interface IAuthState {
+  user: User | null;
+  authenticated: boolean;
+  status: status;
+  error: unknown;
+}
+
+export interface ISignUpData {
+  email: string;
+  password: string;
+  // name: string;
+}
+
+export interface ISignInData {
+  email: string;
+  password: string;
+}
