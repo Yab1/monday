41cfec77e4f1871d32f155e6bef21dcd11003594
Yab1 <yabilisanu@gmail.com>
2023-10-24 21:17:39 +0300

    Commented

--- src/features/auth/AuthRoute.tsx
+++ src/features/auth/AuthRoute.tsx
@@ -1,10 +1,27 @@
 import { ReactNode, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAppSelector } from "@/hooks";
+import { User, getAuth, onAuthStateChanged } from "firebase/auth";
+import { useAppDispatch, useAppSelector } from "@/hooks";
+import { authenticate } from "@/slices";
 
 function AuthRoute({ children }: { children: ReactNode }) {
   const { authenticated } = useAppSelector((state) => state.auth);
+  // const dispatch = useAppDispatch();
   const navigate = useNavigate();
+
+  const auth = getAuth();
+
+  // useEffect(() => {
+  //   AuthCheck();
+  // }, [auth]);
+
+  // const AuthCheck = onAuthStateChanged(auth, (user: User | null) => {
+  //   if (user !== null) {
+  //     if (user.emailVerified) {
+  //       dispatch(authenticate());
+  //     }
+  //   }
+  // });
 
   useEffect(() => {
     if (!authenticated) navigate(-1);
