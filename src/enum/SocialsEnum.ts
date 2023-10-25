d81ecaba9fe9cf6827062880bcc9b7e4d89a5301
Yab1 <yabilisanu@gmail.com>
2023-10-25 20:09:01 +0300

    Defined an icon and color for social media

--- src/enum/SocialsEnum.ts
+++ src/enum/SocialsEnum.ts
@@ -1,9 +1,31 @@
-enum SocialsEnum {
+import {
+  BiLogoFacebook,
+  BiLogoGithub,
+  BiLogoLinkedin,
+  BiLogoTelegram,
+} from "react-icons/bi";
+import { RiTwitterXFill } from "react-icons/ri";
+
+export enum SocialsEnum {
   Facebook = "Facebook",
   GitHub = "GitHub",
   LinkedIn = "LinkedIn",
   Telegram = "Telegram",
-  Twitter = "Twitter",
+  X = "X",
 }
 
-export default SocialsEnum;
+export const SocialIconComponents = {
+  [SocialsEnum.Facebook]: BiLogoFacebook,
+  [SocialsEnum.GitHub]: BiLogoGithub,
+  [SocialsEnum.LinkedIn]: BiLogoLinkedin,
+  [SocialsEnum.Telegram]: BiLogoTelegram,
+  [SocialsEnum.X]: RiTwitterXFill,
+};
+
+export const SocialIconColors = {
+  [SocialsEnum.Facebook]: "#1178f2",
+  [SocialsEnum.GitHub]: "#333333",
+  [SocialsEnum.LinkedIn]: "#0a66c2",
+  [SocialsEnum.Telegram]: "#1DA1F2",
+  [SocialsEnum.X]: "#333333",
+};
