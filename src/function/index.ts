88c17c18de25640cd54c2cc54c6dc1a984d2d77a
Yab1 <yabilisanu@gmail.com>
2023-10-25 20:24:56 +0300

    a function that creates a new user

--- src/function/index.ts
+++ src/function/index.ts
@@ -1,2 +1,3 @@
 export { default as manageAttributes } from "./manageAttributes";
 export { default as getCurrentDate } from "./getCurrentDate";
+export { default as createNewUser } from "./createNewUser";
