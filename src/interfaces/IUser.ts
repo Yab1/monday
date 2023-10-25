b5269a1748136a9fde68bf65c13f4f74683b4a82
Yab1 <yabilisanu@gmail.com>
2023-10-25 17:20:42 +0300

    Cleanup code to avoid dry

---
+++ src/interfaces/IUser.ts
@@ -0,0 +1,22 @@
+import { SettingEnum, SocialsEnum } from "@/enum";
+import { IInvitation, IProjectMember } from "@/interfaces";
+
+interface IUser {
+  id: string;
+  name: string;
+  photoURL: string;
+  email: string;
+  title: string;
+  aboutMe: string;
+  isOnline: boolean;
+  ownedProjects: IProjectMember[];
+  collaboratingProjects: IProjectMember[];
+  invitations: IInvitation[];
+  socials: Record<SocialsEnum, string>;
+  settings: {
+    title: "account" | "application";
+    options: Record<SettingEnum, boolean>;
+  }[];
+}
+
+export default IUser;
