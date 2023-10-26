693cd693486a564003d9168b26e63bf6f7eb2f53
Yab1 <yabilisanu@gmail.com>
2023-10-26 22:30:41 +0300

    Feature: implemented project creating and adding group

--- src/dictionaries/alterDictionary.ts
+++ src/dictionaries/alterDictionary.ts
@@ -1,115 +1,81 @@
+import { addDoc, collection, doc, getFirestore } from "firebase/firestore";
+import { IProjectMetaData, IBatchMetaData } from "@/interfaces";
 import { ActionEnum, TargetEnum } from "@/enum";
-import { Project, User } from "@/interfaces";
+import { app } from "@/firebase";
 
 type ActionEnumFunction = (
-  id: string,
-  data: Project[] | User[],
-  payload?: Object
+  data: IProjectMetaData | IBatchMetaData,
+  projectID?: string,
+  batchID?: string,
+  taskID?: string
 ) => void;
+
+const db = getFirestore(app);
 
 const alterDictionary: Record<
   TargetEnum,
   Record<ActionEnum, ActionEnumFunction>
 > = {
   [TargetEnum.Project]: {
-    [ActionEnum.Add]: (_, data, payload) => {
-      const projects = data as Project[];
-      return [...projects, payload];
-    },
-    [ActionEnum.Delete]: (id, data, _) => {
-      const projects = data as Project[];
-      return projects.filter((project) => project.id !== id);
-    },
-    [ActionEnum.Update]: (id, data, payload) => {
-      return data.map((project) => {
-        if (project.id === id) {
-          return payload;
-        } else {
-          return project;
-        }
-      });
+    [ActionEnum.Add]: async (data) => {
+      const newProject = data as IProjectMetaData;
+      return await addDoc(collection(db, "projects"), newProject);
     },
   },
-  [TargetEnum.Group]: {
-    [ActionEnum.Add]: (id, data, payload) => {
-      const projects = data as Project[];
-      return projects.map((project) => {
-        if (project.id === id) {
-          const updatedGroups = [...project.groups, payload];
-          return { ...project, groups: updatedGroups };
-        } else {
-          return project;
-        }
-      });
-    },
-    [ActionEnum.Delete]: (id, data, _) => {
-      const projects = data as Project[];
-      return projects.map((project) => {
-        const updatedGroups = project.groups.filter((group) => group.id !== id);
-        return { ...project, groups: updatedGroups };
-      });
-    },
-    [ActionEnum.Update]: (id, data, payload) => {
-      const projects = data as Project[];
-      return projects.map((project) => {
-        const updatedGroups = project.groups.map((group) => {
-          if (group.id === id) {
-            return payload;
-          } else {
-            return group;
-          }
-        });
-        return { ...project, groups: updatedGroups };
-      });
+  [TargetEnum.Batch]: {
+    [ActionEnum.Add]: async (data, projectID) => {
+      const parentDocRef = doc(db, "projects", projectID as string);
+      const subCollectionRef = collection(parentDocRef, "subCollection");
+      return await addDoc(subCollectionRef, data);
     },
   },
-  [TargetEnum.Task]: {
-    [ActionEnum.Add]: (id, data, payload) => {
-      const projects = data as Project[];
-      return projects.map((project) => {
-        const updatedGroups = project.groups.map((group) => {
-          if (group.id === id) {
-            const updatedTasks = [...group.tasks, payload];
-            return { ...group, tasks: updatedTasks };
-          } else {
-            return group;
-          }
-        });
-        return { ...project, groups: updatedGroups };
-      });
-    },
-    [ActionEnum.Delete]: (id, data, _) => {
-      const projects = data as Project[];
-      return projects.map((project) => {
-        const updatedGroups = project.groups.map((group) => {
-          const updatedTasks = group.tasks.filter((task) => task.id !== id);
-          return { ...group, tasks: updatedTasks };
-        });
-        return { ...project, groups: updatedGroups };
-      });
-    },
-    [ActionEnum.Update]: (id, data, payload) => {
-      const projects = data as Project[];
-      return projects.map((project) => {
-        const updatedGroups = project.groups.map((group) => {
-          const updatedTasks = group.tasks.map((task) => {
-            if (task.id === id) {
-              return payload;
-            } else {
-              return task;
-            }
-          });
-          return { ...group, tasks: updatedTasks };
-        });
-        return { ...project, groups: updatedGroups };
-      });
-    },
-  },
-  [TargetEnum.Account]: {
-    [ActionEnum.Add]: () => {},
-    [ActionEnum.Delete]: () => {},
-    [ActionEnum.Update]: () => {},
-  },
+  // [TargetEnum.Task]: {
+  //   [ActionEnum.Add]: (id, data, payload) => {
+  //     const projects = data as IProject[];
+  //     return projects.map((project) => {
+  //       const updatedGroups = project.groups.map((group) => {
+  //         if (group.id === id) {
+  //           const updatedTasks = [...group.tasks, payload];
+  //           return { ...group, tasks: updatedTasks };
+  //         } else {
+  //           return group;
+  //         }
+  //       });
+  //       return { ...project, groups: updatedGroups };
+  //     });
+  //   },
+  //   [ActionEnum.Delete]: (id, data, _) => {
+  //     const projects = data as IProject[];
+  //     return projects.map((project) => {
+  //       const updatedGroups = project.groups.map((group) => {
+  //         const updatedTasks = group.tasks.filter((task) => task.id !== id);
+  //         return { ...group, tasks: updatedTasks };
+  //       });
+  //       return { ...project, groups: updatedGroups };
+  //     });
+  //   },
+  //   [ActionEnum.Update]: (id, data, payload) => {
+  //     const projects = data as IProject[];
+  //     return projects.map((project) => {
+  //       const updatedGroups = project.groups.map((group) => {
+  //         const updatedTasks = group.tasks.map((task) => {
+  //           if (task.id === id) {
+  //             return payload;
+  //           } else {
+  //             return task;
+  //           }
+  //         });
+  //         return { ...group, tasks: updatedTasks };
+  //       });
+  //       return { ...project, groups: updatedGroups };
+  //     });
+  //   },
+  // },
+  // [TargetEnum.Account]: {
+  //   [ActionEnum.Add]: () => {},
+  //   [ActionEnum.Delete]: () => {},
+  //   [ActionEnum.Update]: () => {},
+  // },
 };
 
 export default alterDictionary;
