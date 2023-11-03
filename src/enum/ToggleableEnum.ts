22a082d16a9d0e953fa006ed1d1f8be131ea1027
Yab1 <yabilisanu@gmail.com>
2023-11-03 16:38:42 +0300

    Removed Unused enum

--- src/enum/ToggleableEnum.ts
+++ src/enum/ToggleableEnum.ts
@@ -3,7 +3,6 @@ enum ToggleableEnum {
   IS_LOADING = "isLoading",
   SIDE_NAV = "sideNav",
   ALERT = "alert",
-  FIREBASE_INITIALIZED = "firebaseInitialized",
   // EDIT_PROJECT_DIALOG = "editProjectDialog",
   // ADD_PROJECT_DIALOG = "addProjectDialog",
   // ADD_BATCH = "AddGroup",
