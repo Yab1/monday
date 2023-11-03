8f634363d1e89a67f7497aa048b1497f41268be6
Yab1 <yabilisanu@gmail.com>
2023-11-03 16:38:24 +0300

    User authentication implemented

--- src/redux/slices/authSlice.ts
+++ src/redux/slices/authSlice.ts
@@ -1,9 +1,10 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { authWithGoogle, logOut } from "../thunks/authThunks";
-import { IAuthState, status } from "@/interfaces";
+import { authWithGoogle, logOut } from "@/redux/thunks/authThunks";
+import { createUser, readUser } from "@/redux/thunks/crudThunks";
+import { IAuthState, IUser, TStatus } from "@/interfaces";
 
 const initialState: IAuthState = {
-  // user: {} as IUser,
+  user: {} as IUser,
   authenticated: false,
   status: "idle",
   error: null,
@@ -13,24 +14,20 @@ const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    authenticate: (state) => {
-      state.authenticated = true;
-    },
-    updateStatus: (state, action: PayloadAction<status>) => {
+    resetState: () => initialState,
+    updateStatus: (state, action: PayloadAction<TStatus>) => {
       state.status = action.payload;
     },
   },
   extraReducers(builder) {
     builder
-      .addCase(authWithGoogle.pending, (state) => {
+      .addCase(createUser.pending, (state) => {
         state.status = "loading";
       })
-      .addCase(authWithGoogle.fulfilled, (state, _) => {
-        state.status = "succeeded";
-        state.authenticated = true;
+      .addCase(createUser.fulfilled, (state, _) => {
         state.error = null;
       })
-      .addCase(authWithGoogle.rejected, (state, action) => {
+      .addCase(createUser.rejected, (state, action) => {
         state.status = "failed";
         console.log(action.error.code);
         // switch (action.error.code) {
@@ -45,6 +42,48 @@ const authSlice = createSlice({
         //   default:
         //     state.error = "An error occurred. Please try again later.";
         // }
+      })
+      .addCase(readUser.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(readUser.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.error = null;
+        state.user = action.payload;
+        state.authenticated = true;
+      })
+      .addCase(readUser.rejected, (state, action) => {
+        state.status = "failed";
+        console.log(action.error.code);
+        // switch (action.error.code) {
+        //   case "auth/email-already-in-use":
+        //     state.error =
+        //       "This email is already in use. Please sign in or use a different email.";
+        //     break;
+        //   case "auth/network-request-failed":
+        //     state.error =
+        //       "Network request failed. Please check your internet connection and try again.";
+        //     break;
+        //   default:
+        //     state.error = "An error occurred. Please try again later.";
+        // }
+      })
+      .addCase(authWithGoogle.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(authWithGoogle.fulfilled, (state, _) => {
+        state.error = null;
+      })
+      .addCase(authWithGoogle.rejected, (state, action) => {
+        state.status = "failed";
+        switch (action.error.code) {
+          case "auth/internal-error":
+            state.error =
+              "Network request failed. Please check your internet connection and try again.";
+            break;
+          default:
+            state.error = "An error occurred. Please try again later.";
+        }
       })
       .addCase(logOut.pending, (state) => {
         state.status = "loading";
@@ -73,5 +112,5 @@ const authSlice = createSlice({
   },
 });
 
-export const { authenticate, updateStatus } = authSlice.actions;
+export const { updateStatus, resetState } = authSlice.actions;
 export default authSlice.reducer;
