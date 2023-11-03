991db49507b68bb216f23fbf7f8d1ca39b9f2f61
Yab1 <yabilisanu@gmail.com>
2023-11-03 16:36:33 +0300

    Route Guard removed

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,4 +1,3 @@
-export { default as RouteGuard } from "./RouteGuard";
 export { default as SignIn } from "./pages/SignInForm";
 export { default as SignUp } from "./pages/SignUpForm";
 export { default as VerificationPrompt } from "./pages/VerificationPrompt";
