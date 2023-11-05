fa185d49dc7bf9f8fb261dac5aca2dc2a30e2422
Yab1 <yabilisanu@gmail.com>
2023-11-05 18:36:05 +0300

    Added an interface to define private data

--- src/interfaces/IAuth.ts
+++ src/interfaces/IAuth.ts
@@ -1,7 +1,8 @@
-import { IUser, TStatus } from "./index";
+import { IPrivateData, IUser, TStatus } from "./index";
 export interface IAuthState {
   user: IUser;
   authenticated: boolean;
   status: TStatus;
   error: unknown;
+  privateData: IPrivateData;
 }
