1a21e4581db0df1c5035be3f722479f173c4a89c
Yab1 <yabilisanu@gmail.com>
2023-11-05 18:33:58 +0300

    Implemented Route Guard

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,3 +1,4 @@
 export { default as SignIn } from "./pages/SignInForm";
 export { default as SignUp } from "./pages/SignUpForm";
 export { default as VerificationPrompt } from "./pages/VerificationPrompt";
+export { default as RouteGuard } from "./RouteGuard";
