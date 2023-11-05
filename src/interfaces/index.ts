9b00da1f81a9002d7f1e72079cc47e799f03f7ab
Yab1 <yabilisanu@gmail.com>
2023-11-05 18:35:47 +0300

    Removed Deleted Interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,7 +1,6 @@
 export type { default as ChartsConfig } from "./ChartsConfig";
-export type { default as IProjectMember } from "./IProjectMember";
 export type { default as IInvitation } from "./IInvitation";
-export type { default as IAccessControl } from "./IAccessControl";
+export * from "./IAccessControl";
 export * from "./IAuth";
 export * from "./IUser";
 export * from "./DailyProgressChart";
