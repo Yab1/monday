369645ee08811a1e3d861606bc97ac4c388f19cc
Yab1 <yabilisanu@gmail.com>
2023-11-05 18:33:51 +0300

    Implemented Route Guard

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,27 @@
-import { useEffect, useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { onAuthStateChanged, User } from "firebase/auth";
+import { useAppSelector } from "@/hooks";
 import { Auth, Dashboard } from "@/layouts";
-import { useAppDispatch, useAppSelector } from "@/hooks";
-import { resetState, showAlert } from "@/redux/slices";
-import { auth } from "@/firebase";
-import { createUser } from "@/redux/thunks/crudThunks";
-import { Loading } from "@/widgets";
+import { RouteGuard } from "@/features/auth";
 
 function App() {
-  const { error, status, authenticated } = useAppSelector(
-    (state) => state.auth
-  );
-  const [firebase, setFirebase] = useState(false);
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (user: User | null) => {
-      setFirebase(true);
-      if (user !== null) dispatch(createUser({ user }));
-    });
-  }, [auth]);
-
-  useEffect(() => {
-    if (error) {
-      dispatch(showAlert(true));
-
-      setTimeout(() => {
-        dispatch(showAlert(false));
-        dispatch(resetState());
-      }, 4000);
-    }
-  }, [error]);
-
-  if (!firebase || status === "loading") return <Loading />;
+  const { authenticated } = useAppSelector((state) => state.auth);
 
   return (
-    <Routes>
-      <Route path="/dashboard/*" element={<Dashboard />} />
-      <Route path="/auth/*" element={<Auth />} />
-      <Route
-        path="*"
-        element={
-          <Navigate
-            to={authenticated ? "/dashboard/home" : "/auth/sign-in"}
-            replace
-          />
-        }
-      />
-    </Routes>
+    <RouteGuard>
+      <Routes>
+        <Route path="/dashboard/*" element={<Dashboard />} />
+        <Route path="/auth/*" element={<Auth />} />
+        <Route
+          path="*"
+          element={
+            <Navigate
+              to={authenticated ? "/dashboard/home" : "/auth/sign-in"}
+              replace
+            />
+          }
+        />
+      </Routes>
+    </RouteGuard>
   );
 }
 
---
+++ src/features/auth/RouteGuard.tsx
@@ -0,0 +1,63 @@
+import { ReactNode, useEffect, useState } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { useAppDispatch, useAppSelector } from "@/hooks";
+import { resetState, showAlert } from "@/redux/slices";
+import { auth, db } from "@/firebase";
+import {
+  createUser,
+  readPrivateData,
+  readUser,
+} from "@/redux/thunks/crudThunks";
+import { Loading } from "@/widgets";
+import { doc, getDoc } from "firebase/firestore";
+
+function RouteGuard({ children }: { children: ReactNode }) {
+  const { error, status } = useAppSelector((state) => state.auth);
+  const [firebase, setFirebase] = useState(false);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user: User | null) => {
+      setFirebase(true);
+      if (user !== null) {
+        const userRef = doc(db, "users", user.uid);
+        const userDocSnapshot = await getDoc(userRef);
+
+        if (userDocSnapshot.exists()) {
+          dispatch(readUser(user.uid));
+          dispatch(readPrivateData(user.uid));
+        } else {
+          dispatch(createUser({ user }));
+        }
+      }
+    });
+  }, [dispatch, auth]);
+
+  // const unsubscribe = async () => {
+  //   const abortController = new AbortController();
+
+  //   return () => {
+  //     abortController.abort();
+  //   };
+  // };
+  // return () => {
+  //   unsubscribe();
+  // };
+
+  useEffect(() => {
+    if (error) {
+      dispatch(showAlert(true));
+
+      setTimeout(() => {
+        dispatch(showAlert(false));
+        dispatch(resetState());
+      }, 4000);
+    }
+  }, [error]);
+
+  return !firebase || status === "loading" ? <Loading /> : children;
+}
+
+RouteGuard.displayName = "/src/features/auth/RouteGuard.tsx";
+
+export default RouteGuard;
