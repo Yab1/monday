fc75b022e355998563cacdf78f87c6faf14f820f
Yab1 <yabilisanu@gmail.com>
2023-11-07 16:47:52 +0300

    Redux saga defined

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 import { rootReducer } from "./rootReducer";
+import rootSaga from "./rootSaga";
+
+const sagaMiddleWare = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false })
+      .concat(sagaMiddleWare)
       .concat(thunk)
       .concat(logger),
 });
+
+sagaMiddleWare.run(rootSaga);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
