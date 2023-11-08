9a0fb84538a53e36c1546cc656b5cb978469b112
Yab1 <yabilisanu@gmail.com>
2023-11-08 22:25:03 +0300

    Implemented Guard using saga with channel

--- src/features/auth/RouteGuard.tsx
+++ src/features/auth/RouteGuard.tsx
@@ -2,42 +2,27 @@ import { ReactNode, useEffect, useState } from "react";
 import { onAuthStateChanged, User } from "firebase/auth";
 import { useAppDispatch, useAppSelector } from "@/hooks";
 import { resetState, showAlert } from "@/redux/slices";
-import { auth, db } from "@/firebase";
-import {
-  createUser,
-  readPrivateData,
-  readUser,
-} from "@/redux/thunks/crudThunks";
+import { auth } from "@/firebase";
 import { Loading } from "@/widgets";
-import { doc, getDoc } from "firebase/firestore";
+import { SagaActions } from "@/enum";
 
 function RouteGuard({ children }: { children: ReactNode }) {
-  const { error, status } = useAppSelector((state) => state.auth);
   const [firebase, setFirebase] = useState(false);
+  const { error, status } = useAppSelector((state) => state.progress);
   const dispatch = useAppDispatch();
 
-  // useEffect(() => {
-  //   const fetchData = async (user: User | null) => {
-  //     setFirebase(true);
-  //     if (user !== null) {
-  //       const userRef = doc(db, "users", user.uid);
-  //       const userDocSnapshot = await getDoc(userRef);
+  useEffect(() => {
+    const fetchData = async (user: User | null) => {
+      setFirebase(true);
+      if (user !== null) {
+        dispatch({ type: SagaActions.INITIALIZE_USER, payload: user });
+      }
+    };
 
-  //       if (userDocSnapshot.exists()) {
-  //         dispatch(readUser(user.uid));
-  //         dispatch(readPrivateData(user.uid));
-  //       } else {
-  //         dispatch(createUser({ user }));
-  //         dispatch(readUser(user.uid));
-  //         dispatch(readPrivateData(user.uid));
-  //       }
-  //     }
-  //   };
-
-  //   onAuthStateChanged(auth, (user: User | null) => {
-  //     fetchData(user);
-  //   });
-  // }, [dispatch, auth]);
+    onAuthStateChanged(auth, (user: User | null) => {
+      fetchData(user);
+    });
+  }, [dispatch, auth]);
 
   useEffect(() => {
     if (error) {
@@ -50,8 +35,7 @@ function RouteGuard({ children }: { children: ReactNode }) {
     }
   }, [error]);
 
-  // return !firebase || status === "loading" ? <Loading /> : children;
-  return false ? <Loading /> : children;
+  return !firebase || status === "Loading" ? <Loading /> : children;
 }
 
 RouteGuard.displayName = "/src/features/auth/RouteGuard.tsx";
