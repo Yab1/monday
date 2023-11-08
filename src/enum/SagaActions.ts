832572295efb48d30ff99a6d43043ed819385537
Yab1 <yabilisanu@gmail.com>
2023-11-08 22:24:09 +0300

    Added new saga actions

--- src/enum/SagaActions.ts
+++ src/enum/SagaActions.ts
@@ -1,5 +1,16 @@
 enum SagaActions {
   AUTH_WITH_GOOGLE = "auth/with-google",
+  LOG_OUT = "auth/log-out",
+  INITIALIZE_USER = "user/initialize-user",
+
+  // USER CRUD
+  CREATE_USER = "user/create-user",
+  READ_USER = "user/read-user",
+  UPDATE_USER = "user/update-user",
+  DELETE_USER = "user/delete-user",
+
+  // PRIVATE DATA CRUD
+  READ_PVT = "user/read-pvt",
 }
 
 export default SagaActions;
