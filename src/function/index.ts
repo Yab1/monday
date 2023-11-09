3c75b9513ed5d501e0702f4b75e32205aa7c0af9
Yab1 <yabilisanu@gmail.com>
2023-11-09 12:40:05 +0300

    Function that map error code to custom message

--- src/function/index.ts
+++ src/function/index.ts
@@ -2,3 +2,5 @@ export { default as manageAttributes } from "./manageAttributes";
 export { default as getCurrentDate } from "./getCurrentDate";
 export { default as createNewUser } from "./createNewUser";
 export { default as createNewProject } from "./createNewProject";
+export { default as deriveAuthError } from "./deriveAuthError";
+export { default as deriveFirestoreError } from "./deriveFirestoreError";
