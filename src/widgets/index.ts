a1841669c33c69bc69834e0aff2749600ca5c9ab
Yab1 <yabilisanu@gmail.com>
2023-11-09 18:52:55 +0300

    component to display on app initialization

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,4 +1,5 @@
 export { default as Loading } from "./layout/Loading";
+export { default as SplashScreen } from "./layout/SplashScreen";
 // export { default as StatisticsChart } from "./charts/StatisticsChart";
 export { default as ProjectOverviewTable } from "./tables/ProjectOverviewTable";
 export { default as ConfirmationDialog } from "./dialog/ConfirmationDialog";
