801fb3447ac040eb9bdf5361b98b86566f0e7c31
Yab1 <yabilisanu@gmail.com>
2023-11-09 12:45:45 +0300

    Added a catch block with error driver function

--- src/function/index.ts
+++ src/function/index.ts
@@ -4,3 +4,5 @@ export { default as createNewUser } from "./createNewUser";
 export { default as createNewProject } from "./createNewProject";
 export { default as deriveAuthError } from "./deriveAuthError";
 export { default as deriveFirestoreError } from "./deriveFirestoreError";
+export { default as generateUserData } from "./generateUserData";
+export { default as generateUserSettings } from "./generateUserSettings";
