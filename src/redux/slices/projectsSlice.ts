c3f22fa58915ec192bb6b4b5a792979a07892b40
Yab1 <yabilisanu@gmail.com>
2023-11-09 12:46:08 +0300

    Removed thunk and externalReducers

--- src/redux/slices/projectsSlice.ts
+++ src/redux/slices/projectsSlice.ts
@@ -1,26 +1,10 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { IProjectState } from "@/interfaces";
-import { readProject } from "../thunks/crudThunks";
 
 export const projectSlice = createSlice({
   name: "project-manager",
   initialState: {} as IProjectState,
   reducers: {},
-  extraReducers(builder) {
-    builder
-      .addCase(readProject.pending, (state) => {
-        state.status = "loading";
-      })
-      .addCase(readProject.fulfilled, (state, action) => {
-        state.status = "succeeded";
-        state.error = null;
-        // console.log(action.payload);
-      })
-      .addCase(readProject.rejected, (state, action) => {
-        state.status = "failed";
-        console.log(action.error.code);
-      });
-  },
 });
 
 export default projectSlice.reducer;
