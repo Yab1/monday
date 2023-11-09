b20dfebec0b1ffc1f6ca2109f27b9dfc6640923f
Yab1 <yabilisanu@gmail.com>
2023-11-09 09:30:41 +0300

    Cleanup code

--- src/redux/sagas/MediatorSagas/initializeUser.ts
+++ src/redux/sagas/MediatorSagas/initializeUser.ts
@@ -1,14 +1,13 @@
 import { SagaActions } from "@/enum";
-import { call, fork, takeLatest } from "redux-saga/effects";
+import { all, call, fork, takeLatest } from "redux-saga/effects";
 import { createUser, readUser, readPrivateData } from "@/redux/sagas/crudSagas";
 import { User } from "firebase/auth";
 
 function* initializeUser(action: { type: string; payload: User }) {
   const user = action.payload;
-  yield call(createUser, user);
 
-  yield fork(readUser, user.uid);
-  yield fork(readPrivateData, user.uid);
+  yield call(createUser, user);
+  yield all([fork(readUser, user.uid), fork(readPrivateData, user.uid)]);
 }
 
 function* watchInitializeUser() {
