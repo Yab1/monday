30fe3d4b90f4034cc7782e18cc0b5a9df5cfc89a
Yab1 <yabilisanu@gmail.com>
2023-11-09 18:47:02 +0300

    Cleanup and removed unused code

--- src/features/auth/RouteGuard.tsx
+++ src/features/auth/RouteGuard.tsx
@@ -1,38 +1,28 @@
 import { ReactNode, useEffect, useState } from "react";
 import { onAuthStateChanged, User } from "firebase/auth";
 import { useAppDispatch, useAppSelector } from "@/hooks";
-import {
-  authenticate,
-  progressStart,
-  progressSuccess,
-  resetProgressState,
-  showAlert,
-} from "@/redux/slices";
+import { authIdle, authStart, authSucceeded } from "@/redux/slices";
 import { auth } from "@/firebase";
-import { Loading } from "@/widgets";
+import { SplashScreen } from "@/widgets";
 import { SagaActions, StatusEnum } from "@/enum";
 import { isObjectEmpty } from "@/function";
 
 function RouteGuard({ children }: { children: ReactNode }) {
   const [firebase, setFirebase] = useState(false);
-  const { error, status } = useAppSelector((state) => state.progress);
-  const { user, privateData } = useAppSelector((state) => state.auth);
+  const { authStatus, user, privateData } = useAppSelector(
+    (state) => state.auth
+  );
   const dispatch = useAppDispatch();
-
-  // if (status === StatusEnum.LOADING || status === StatusEnum.IDLE) {
-  //   // console.log(status);
-  // }
-  // console.log(status);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user: User | null) => {
       setFirebase(true);
-      dispatch(progressStart());
+      dispatch(authStart());
 
       if (user !== null) {
         dispatch({ type: SagaActions.INITIALIZE_USER, payload: user });
       } else {
-        dispatch(progressSuccess());
+        dispatch(authIdle());
       }
     });
 
@@ -41,23 +31,15 @@ function RouteGuard({ children }: { children: ReactNode }) {
 
   useEffect(() => {
     if (!isObjectEmpty(user) && !isObjectEmpty(privateData)) {
-      dispatch(authenticate(true));
-      dispatch(progressSuccess());
+      dispatch(authSucceeded());
     }
   }, [user, privateData]);
 
-  useEffect(() => {
-    if (error) {
-      dispatch(showAlert(true));
-
-      setTimeout(() => {
-        dispatch(showAlert(false));
-        dispatch(resetProgressState());
-      }, 4000);
-    }
-  }, [error]);
-
-  return !firebase || status === StatusEnum.LOADING ? <Loading /> : children;
+  return !firebase || authStatus === StatusEnum.LOADING ? (
+    <SplashScreen />
+  ) : (
+    children
+  );
 }
 
 RouteGuard.displayName = "/src/features/auth/RouteGuard.tsx";
