cf076ba4ff37abd004d1edee1139ccd83244633f
Yab1 <yabilisanu@gmail.com>
2023-11-09 16:48:22 +0300

    Added a authStatus and an action to control the state

--- src/interfaces/IAuth.ts
+++ src/interfaces/IAuth.ts
@@ -1,6 +1,8 @@
+import { StatusEnum } from "@/enum";
 import { IPrivateData, IUser } from "@/interfaces";
 export interface IAuthState {
   authenticated: boolean;
   user: IUser;
   privateData: IPrivateData;
+  authStatus: StatusEnum;
 }
--- src/redux/slices/authSlice.ts
+++ src/redux/slices/authSlice.ts
@@ -1,29 +1,46 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IAuthState, IPrivateData, IUser } from "@/interfaces";
+import { StatusEnum } from "@/enum";
 
 const initialState: IAuthState = {
   user: {} as IUser,
   privateData: {} as IPrivateData,
   authenticated: false,
+  authStatus: StatusEnum.IDLE,
 };
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    resetAuthState: () => initialState,
+    setStatus: (state, action: PayloadAction<StatusEnum>) => {
+      switch (action.payload) {
+        case StatusEnum.IDLE:
+          return initialState;
+        case StatusEnum.LOADING:
+          state.authStatus = action.payload;
+          state.authenticated = false;
+          break;
+        case StatusEnum.SUCCEEDED:
+          state.authenticated = true;
+          state.authStatus = action.payload;
+          break;
+        case StatusEnum.FAILED:
+          state.authenticated = false;
+          state.authStatus = action.payload;
+          break;
+        default:
+          break;
+      }
+    },
     setUser: (state, action: PayloadAction<IUser>) => {
       state.user = action.payload;
     },
     setPrivateData: (state, action: PayloadAction<IPrivateData>) => {
       state.privateData = action.payload;
     },
-    authenticate: (state, action: PayloadAction<boolean>) => {
-      state.authenticated = action.payload;
-    },
   },
 });
 
-export const { resetAuthState, setUser, setPrivateData, authenticate } =
-  authSlice.actions;
+export const { setStatus, setUser, setPrivateData } = authSlice.actions;
 export default authSlice.reducer;
