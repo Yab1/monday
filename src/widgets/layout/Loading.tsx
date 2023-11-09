d64f63634407e3e3a56c464a72a53ac3f4e94048
Yab1 <yabilisanu@gmail.com>
2023-11-09 16:02:23 +0300

    Added a ternary to control component rendering

--- src/widgets/layout/Loading.tsx
+++ src/widgets/layout/Loading.tsx
@@ -1,27 +1,26 @@
 import { Fragment } from "react";
 import { useAppSelector } from "@/hooks";
+import { StatusEnum } from "@/enum";
 
 function Loading() {
-  const {
-    toggleable: { isLoading },
-  } = useAppSelector((state) => state.ui);
+  const { status } = useAppSelector((state) => state.progress);
 
   return (
     <Fragment>
-      <div className="fixed inset-0 z-50 grid w-screen h-screen bg-black bg-opacity-50 place-items-center backdrop-blur-sm">
-        <div className="wrapper">
-          <div className="box-wrap">
-            <div className="box one"></div>
-            <div className="box two"></div>
-            <div className="box three"></div>
-            <div className="box four"></div>
-            <div className="box five"></div>
-            <div className="box six"></div>
+      {status === StatusEnum.LOADING || status === StatusEnum.IDLE ? (
+        <div className="fixed inset-0 z-50 grid w-screen h-screen bg-black bg-opacity-50 place-items-center backdrop-blur-sm">
+          <div className="wrapper">
+            <div className="box-wrap">
+              <div className="box one"></div>
+              <div className="box two"></div>
+              <div className="box three"></div>
+              <div className="box four"></div>
+              <div className="box five"></div>
+              <div className="box six"></div>
+            </div>
           </div>
         </div>
-      </div>
-      {/* {isLoading ? (
-      ) : null} */}
+      ) : null}
     </Fragment>
   );
 }
