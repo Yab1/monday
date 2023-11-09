e0a0e84f8f36c86cc0963f4870b85d9f6c97537c
Yab1 <yabilisanu@gmail.com>
2023-11-09 12:42:17 +0300

    Added create project

--- src/redux/rootSaga.ts
+++ src/redux/rootSaga.ts
@@ -1,9 +1,15 @@
 import { all } from "redux-saga/effects";
 import { authWithGoogleSaga, logOutSaga } from "./sagas/authSagas";
-import { initializeUserSaga } from "./sagas/MediatorSagas";
+import { initializeUser } from "./sagas/MediatorSagas";
+import { createProject } from "./sagas/crudSagas";
 
 function* rootSaga() {
-  yield all([...authWithGoogleSaga, ...logOutSaga, ...initializeUserSaga]);
+  yield all([
+    ...authWithGoogleSaga,
+    ...logOutSaga,
+    ...initializeUser,
+    ...createProject,
+  ]);
 }
 
 export default rootSaga;
