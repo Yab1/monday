c00365f028f886c67254861e4e42c905a10d3784
Yab1 <yabilisanu@gmail.com>
2023-11-11 13:33:47 +0300

    Refactor: Extend IFirestoreState interface.

--- src/interfaces/IFirestore.ts
+++ src/interfaces/IFirestore.ts
@@ -1,8 +1,14 @@
 import { StatusEnum } from "@/enum";
+import { IInvitation, IProject, IUser, IUserSettings } from ".";
 
 interface IFirestoreState {
   firestoreStatus: StatusEnum;
   firestoreError: unknown;
+  user: IUser;
+  settings: IUserSettings;
+  projects: IProject[];
+  invitations: IInvitation[];
+  messages: null;
 }
 
 export default IFirestoreState;
