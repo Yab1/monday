04d379436ecd122fb29dcd98b5e54d3f512e1b04
Yab1 <yabilisanu@gmail.com>
2023-11-11 13:28:29 +0300

    Refactor: Simplify IAuthState interface.

--- src/interfaces/IAuth.ts
+++ src/interfaces/IAuth.ts
@@ -1,9 +1,7 @@
 import { StatusEnum } from "@/enum";
-import { IPrivateData, IUser } from "@/interfaces";
+
 export interface IAuthState {
   authStatus: StatusEnum;
   authError: string | null;
   authenticated: boolean;
-  user: IUser;
-  privateData: IPrivateData;
 }
