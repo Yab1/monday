35fb436c06a936a4400a339f22bdd898d152de57
Yab1 <yabilisanu@gmail.com>
2023-11-11 13:59:06 +0300

    Added readSetting and readProjectId trigger

--- src/redux/sagas/MediatorSagas/initializeUser.ts
+++ src/redux/sagas/MediatorSagas/initializeUser.ts
@@ -3,17 +3,22 @@ import { all, call, fork, takeLatest } from "redux-saga/effects";
 import {
   createUserSaga,
   readUserSaga,
-  readPrivateDataSaga,
+  readSettingsSaga,
+  readProjectId,
 } from "@/redux/sagas/crudSagas";
 import { User } from "firebase/auth";
 
-function* initializeUserSaga(action: { type: string; payload: User }) {
+function* initializeUserSaga(action: {
+  type: SagaActions.INITIALIZE_USER;
+  payload: User;
+}) {
   const user = action.payload;
 
   yield all([
     call(createUserSaga, user),
     fork(readUserSaga, user.uid),
-    fork(readPrivateDataSaga, user.uid),
+    fork(readSettingsSaga, user.uid),
+    fork(readProjectId, user.uid),
   ]);
 }
 
