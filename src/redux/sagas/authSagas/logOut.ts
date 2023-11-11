11df03f411608cc2fda118652a5c07f21c715cd6
Yab1 <yabilisanu@gmail.com>
2023-11-11 13:10:07 +0300

    Added store cleaners

--- src/redux/sagas/authSagas/logOut.ts
+++ src/redux/sagas/authSagas/logOut.ts
@@ -1,6 +1,11 @@
 import { signOut } from "firebase/auth";
-import { call, fork, put, takeLatest } from "redux-saga/effects";
-import { authFailed } from "@/redux/slices";
+import { all, call, fork, put, takeLatest } from "redux-saga/effects";
+import {
+  authFailed,
+  authIdle,
+  cleanBuffer,
+  firestoreIdle,
+} from "@/redux/slices";
 import { FirebaseError } from "firebase/app";
 import { auth } from "@/firebase";
 import { SagaActions } from "@/enum";
@@ -8,7 +13,13 @@ import { deriveAuthError } from "@/function";
 
 function* logOut() {
   try {
-    yield call(signOut, auth);
+    yield all([
+      put({ type: SagaActions.CANCEL_SAGA }),
+      call(signOut, auth),
+      put(firestoreIdle()),
+      put(cleanBuffer()),
+      put(authIdle()),
+    ]);
   } catch (error) {
     if (error instanceof FirebaseError) {
       const errorMessage: string = yield call(deriveAuthError, error.code);
